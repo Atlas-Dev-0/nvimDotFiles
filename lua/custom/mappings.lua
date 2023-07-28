43c6d319d0796d54a42a4e5cfbdd1d2693e90de4
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2023-07-28 19:09:48 +0800

    fixed shift arrow annoyance

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -7,14 +7,22 @@ M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-a>"] = { "ggVG", "select all of the lines of code" },
+    ["<S-Up>"] = {"<Nop>", opts = { noremap = true, silent = true }},
+    ["<S-Down>"] = {"<Nop>", opts = { noremap = true, silent = true }},
   },
+  v = {
+    ["<S-Up>"] = {"<Nop>", opts = { noremap = true, silent = true }},
+    ["<S-Down>"] = {"<Nop>", opts = { noremap = true, silent = true }},
+  }
+  
+
 }
 
+vim.api.nvim_set_keymap('n', '<S-Up>', '<Nop>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<S-Down>', '<Nop>', { noremap = true, silent = true })
 
 
 -- more keybinds!
-
-
 return M
 
 
