441f6a602cd6f5aa276ff2256f281d4c6865d1f7
Atlas-Dev-0 <kenneth.gonzales@bicol-u.edu.ph>
2023-08-11 01:29:19 +0800

    Added BlackHole Register HIKHOK

--- lua/custom/mappings.lua
+++ lua/custom/mappings.lua
@@ -1,30 +1,20 @@
 ---@type MappingsTable
 local M = {}
 
-
-
 M.general = {
   n = {
     [";"] = { ":", "enter command mode", opts = { nowait = true } },
     ["<C-a>"] = { "ggVG", "select all of the lines of code" },
-    ["<S-Up>"] = {"<Nop>", opts = { noremap = true, silent = true }},
-    ["<S-Down>"] = {"<Nop>", opts = { noremap = true, silent = true }},
+    ["<S-Up>"] = { "<Nop>", opts = { noremap = true, silent = true } },
+    ["<S-Down>"] = { "<Nop>", opts = { noremap = true, silent = true } },
+    ["x"] = { '"_d', opts = { noremap = true, silent = true } },
   },
-  v = {
-    ["<S-Up>"] = {"<Nop>", opts = { noremap = true, silent = true }},
-    ["<S-Down>"] = {"<Nop>", opts = { noremap = true, silent = true }},
-  }
-  
 
+  v = {
+    ["x"] = { '"_d', opts = { noremap = true, silent = true } },
+    ["<S-Up>"] = { "<Nop>", opts = { noremap = true, silent = true } },
+    ["<S-Down>"] = { "<Nop>", opts = { noremap = true, silent = true } },
+  },
 }
 
-vim.api.nvim_set_keymap('n', '<S-Up>', '<Nop>', { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<S-Down>', '<Nop>', { noremap = true, silent = true })
-
-
--- more keybinds!
 return M
-
-
-
-
